7ab58314bab622ea67a535fc5666ab961e948122
imehlee <150583492+imehlee@users.noreply.github.com>
2023-11-15 06:20:23 +0900

    wet/dry

--- main.js
+++ main.js
@@ -30,10 +30,12 @@ const moveHandler = (e) => {
 const wetFadeIn = (e) => {
   wet2.gain.value = 0.01;
   wet2.gain.linearRampToValueAtTime(1.2, ctx.currentTime + 0.5);
+  dry2.gain.linearRampToValueAtTime(0.4, ctx.currentTime + 1);
 }
 
 const wetFadeOut = (e) => {
     //wet2.gain.linearRampToValueAtTime(0.01, ctx.currentTime + 0.1);
+    dry2.gain.linearRampToValueAtTime(1.0, ctx.currentTime + 1);
     wet2.gain.value = 0.01;
 }
 
